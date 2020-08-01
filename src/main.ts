34d8c9bc712743f25684295190473bf0c9cffdcf
Vishwanath <vishwanathph@gmail.com>
2020-08-01 13:12:25 +0530

    swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // add this line
+  const options = new DocumentBuilder()
+    .setTitle('Lawn Mowers API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
